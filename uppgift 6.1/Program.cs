738927ad3b0c9245c0c22b430c587738926bbeb3
Admin <Admin@TEB123008>
2023-05-12 08:41:08 +0200

    Add project files.

---
+++ uppgift 6.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class program
+{
+    static int Addera(int tal1, int tal2, int tal3)
+    {
+        return tal1 + tal2 + tal3;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Addera(9,3,6));
+    }
+}
